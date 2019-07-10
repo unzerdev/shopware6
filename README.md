439df8bfd63c5bba27e63eaadca90c4197b79e41
Niklas Gödde <n.goedde@kellerkinder.de>
2019-07-10 11:43:39 +0200

    [HEIDELPAY-57] Add base payment installer

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 Heidelpay payment integration for Shopware 6 including the following payment methods:
 * Credit Card
 * Invoice
-
--- src/HeidelPayment.php
+++ src/HeidelPayment.php
@@ -4,9 +4,44 @@ declare(strict_types=1);
 
 namespace HeidelPayment;
 
+use HeidelPayment\Installers\PaymentInstaller;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
 class HeidelPayment extends Plugin
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function install(InstallContext $installContext): void
+    {
+        (new PaymentInstaller($this->container->get('payment_method.repository')))->install($installContext);
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(ActivateContext $activateContext): void
+    {
+        (new PaymentInstaller($this->container->get('payment_method.repository')))->activate($activateContext);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        (new PaymentInstaller($this->container->get('payment_method.repository')))->deactivate($deactivateContext);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        (new PaymentInstaller($this->container->get('payment_method.repository')))->uninstall($uninstallContext);
+    }
 }
