3dab61e6884317682e5656ad03ad49d0cb3321eb
Jochen Manz <jochen.manz@gmx.de>
2019-07-29 14:17:40 +0200

    [HEIDELPAY-53] correct custom field name constants

--- src/Components/PaymentHandler/AbstractHeidelpayHandler.php
+++ src/Components/PaymentHandler/AbstractHeidelpayHandler.php
@@ -156,7 +156,7 @@ abstract class AbstractHeidelpayHandler implements AsynchronousPaymentHandlerInt
         SalesChannelContext $salesChannelContext
     ): void {
         $customFields = $transaction->getOrderTransaction()->getCustomFields() ?? [];
-        $customFields = array_merge($customFields, [CustomFieldInstaller::CUSTOM_FIELD_TRAHSACTION => true]);
+        $customFields = array_merge($customFields, [CustomFieldInstaller::HEIDELPAY_TRANSACTION => true]);
 
         $update = [
             'id'           => $transaction->getOrderTransaction()->getId(),
