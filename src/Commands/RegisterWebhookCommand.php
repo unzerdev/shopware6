64f1433c69bcbff5959b95d720e9c8635b8a5925
Jochen Manz <jochen.manz@gmx.de>
2019-07-31 18:06:51 +0200

    [HEIDELPAY-51] optimize webhook register command

--- src/Commands/RegisterWebhookCommand.php
+++ src/Commands/RegisterWebhookCommand.php
@@ -7,6 +7,7 @@ namespace HeidelPayment\Commands;
 use HeidelPayment\Components\ClientFactory\ClientFactoryInterface;
 use heidelpayPHP\Exceptions\HeidelpayApiException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -36,7 +37,10 @@ class RegisterWebhookCommand extends Command
     protected function configure()
     {
         $this->setName('heidelpay:register-webhooks');
+        $this->setDescription('registers the heidelpay webhook');
+        $this->addArgument('host', InputArgument::REQUIRED, 'Main Host of the shop. Example: http://www.domain.de');
     }
+
 
     /**
      * {@inheritdoc}
@@ -48,6 +52,18 @@ class RegisterWebhookCommand extends Command
         try {
             $client = $this->clientFactory->createClient();
             $client->deleteAllWebhooks();
+
+            $host = parse_url($input->getArgument('host'));
+
+            if (empty($host['host']) || empty($host['scheme'])) {
+                $style->warning('the provided host is invalid.');
+
+                return;
+            }
+
+            $context = $this->router->getContext();
+            $context->setHost($host['host']);
+            $context->setScheme($host['scheme']);
 
             $url = $this->router->generate('heidelpay_webhook', [], UrlGeneratorInterface::ABSOLUTE_URL);
 
