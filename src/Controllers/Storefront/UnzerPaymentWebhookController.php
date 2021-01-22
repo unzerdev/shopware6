4d04622a79bd82ca47c02da996823331a11b8f5c
a.wink <a.wink@kellerkinder.de>
2021-01-22 11:23:21 +0100

    [HEIDELPAYSUPPORT-51] Fix codestyle

--- src/Controllers/Storefront/UnzerPaymentWebhookController.php
+++ src/Controllers/Storefront/UnzerPaymentWebhookController.php
@@ -10,7 +10,6 @@ use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Throwable;
 use Traversable;
