30e3a08e68333b9f56e5af3b2fa2eafcb0fa226f
Joachim Krempel <j.krempel@kellerkinder.de>
2021-05-07 15:54:09 +0200

    [HEIDELPAYSUPPORT-99] readd unzer transaction in administration order list

--- src/Resources/app/administration/src/module/unzer-payment/extension/sw-order-list/index.js
+++ src/Resources/app/administration/src/module/unzer-payment/extension/sw-order-list/index.js
@@ -1,4 +1,25 @@
+import template from './sw-order-list.html.twig';
 
 Shopware.Component.override('sw-order-list', {
+    template,
 
+    computed: {
+        orderColumns() {
+            return this.getEnhancedOrderColumns();
+        }
+    },
+
+    methods: {
+        getEnhancedOrderColumns() {
+            const baseColumns = this.getOrderColumns();
+
+            baseColumns.splice(1, 0, {
+                property: 'unzerPaymentTransactionId',
+                label: 'unzer-payment.order-list.transactionId',
+                allowResize: true
+            });
+
+            return baseColumns;
+        }
+    }
 });
