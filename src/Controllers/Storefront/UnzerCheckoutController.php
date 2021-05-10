9450c2bfbf3f46f2247d8406a1173e81eedd49fa
Joachim Krempel <j.krempel@kellerkinder.de>
2021-05-10 09:45:31 +0200

    [HEIDELPAYSUPPORT-99] handle changed signature of finishPage action

--- src/Controllers/Storefront/UnzerCheckoutController.php
+++ src/Controllers/Storefront/UnzerCheckoutController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace UnzerPayment6\Controllers\Storefront;
 
+use ArgumentCountError;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
@@ -79,7 +80,7 @@ class UnzerCheckoutController extends StorefrontController
         }
     }
 
-    public function finishPage(Request $request, SalesChannelContext $context, RequestDataBag $dataBag): Response
+    public function finishPage(Request $request, SalesChannelContext $context, RequestDataBag $dataBag = null): Response
     {
         if (!$context->getCustomer()) {
             return $this->redirectToRoute('frontend.checkout.register.page');
@@ -113,6 +114,15 @@ class UnzerCheckoutController extends StorefrontController
             );
         }
 
-        return $this->innerService->finishPage($request, $context, $dataBag);
+        try {
+            /** @phpstan-ignore-next-line */
+            $response = $this->innerService->finishPage($request, $context, $dataBag);
+        } catch (ArgumentCountError $exception) {
+            // signature changed with v6.4, this is the old version without dataBag Parameter
+            /** @phpstan-ignore-next-line */
+            $response = $this->innerService->finishPage($request, $context);
+        }
+
+        return $response;
     }
 }
