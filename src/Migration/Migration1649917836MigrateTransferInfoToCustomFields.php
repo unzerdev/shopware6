36fe5583bd4782c1d80ae51f1576a22978f2433d
Jan-Erik Spreng <j.spreng@kellerkinder.de>
2022-04-14 10:32:53 +0200

    [HEIDELPAYSUPPORT-229] Fix phpstan errors

--- src/Migration/Migration1649917836MigrateTransferInfoToCustomFields.php
+++ src/Migration/Migration1649917836MigrateTransferInfoToCustomFields.php
@@ -18,13 +18,13 @@ class Migration1649917836MigrateTransferInfoToCustomFields extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        $transferInfos = $connection->executeQuery('SELECT * FROM unzer_payment_transfer_info')->fetchAllAssociative();
+        $transferInfos = $connection->executeQuery('SELECT * FROM unzer_payment_transfer_info')->fetchAll();
 
         foreach ($transferInfos as $transferInfo) {
             $infoId        = $transferInfo['id'];
             $transactionId = $transferInfo['transaction_id'];
 
-            $customFields = json_decode($connection->executeQuery('SELECT custom_fields FROM order_transaction WHERE id = ?', [$transactionId])->fetchOne(), true);
+            $customFields = json_decode($connection->executeQuery('SELECT custom_fields FROM order_transaction WHERE id = ?', [$transactionId])->fetchColumn(), true);
 
             if (!array_key_exists(CustomFieldInstaller::UNZER_PAYMENT_TRANSFER_INFO, $customFields)) {
                 unset(
@@ -39,19 +39,19 @@ class Migration1649917836MigrateTransferInfoToCustomFields extends MigrationStep
 
                 $customFields = array_merge($customFields, [CustomFieldInstaller::UNZER_PAYMENT_TRANSFER_INFO => $transferInfo]);
 
-                if ($connection->executeStatement('UPDATE order_transaction SET custom_fields = ? WHERE id = ?', [json_encode($customFields), $transactionId]) !== 1) {
+                if ($connection->exec('UPDATE order_transaction SET custom_fields = ? WHERE id = ?', [json_encode($customFields), $transactionId]) !== 1) {
                     throw new \RuntimeException(sprintf('Can not migrate transfer info %s', Uuid::fromBytesToHex($infoId)));
                 }
             }
 
-            $connection->executeStatement('DELETE FROM unzer_payment_transfer_info WHERE id = ?', [$infoId]);
+            $connection->exec('DELETE FROM unzer_payment_transfer_info WHERE id = ?', [$infoId]);
         }
 
-        if ($connection->executeQuery('SELECT COUNT(*) FROM unzer_payment_transfer_info')->fetchOne() !== '0') {
+        if ($connection->executeQuery('SELECT COUNT(*) FROM unzer_payment_transfer_info')->fetchColumn() !== '0') {
             throw new \RuntimeException('Database table "unzer_payment_transfer_info" is not empty');
         }
 
-        $connection->executeStatement('DROP TABLE unzer_payment_transfer_info');
+        $connection->exec('DROP TABLE unzer_payment_transfer_info');
     }
 
     public function updateDestructive(Connection $connection): void
