190ba06e4f5ac44d9d9ed7eef7ffe6fcd1645d65
Jan-Erik Spreng <j.spreng@kellerkinder.de>
2022-05-02 10:53:23 +0200

    [noticket] Fix syntax

--- src/Components/PaymentHandler/Exception/UnzerPaymentProcessException.php
+++ src/Components/PaymentHandler/Exception/UnzerPaymentProcessException.php
@@ -24,7 +24,7 @@ class UnzerPaymentProcessException extends AsyncPaymentProcessException
 
         parent::__construct(
             $orderId,
-            $apiException->getMerchantMessage(),
+            $apiException->getMerchantMessage()
         );
     }
 
