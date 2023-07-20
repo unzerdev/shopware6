f7792fa718ad3171db5828c34b947b9d9aa721e3
Sebastian König <s.koenig@kellerkinder.de>
2023-07-20 13:31:35 +0200

    feat: [HEIDELPAYSUPPORT-323] remove ci for shopware 6.3

--- src/Components/BasketConverter/BasketConverter.php
+++ src/Components/BasketConverter/BasketConverter.php
@@ -32,8 +32,8 @@ class BasketConverter implements BasketConverterInterface
 
         $amountNet = (float) $item['amountGross'] / (1 + $vat);
 
-        $item['amountVat']     = round($amountNet * $vat, UnzerPayment6::MAX_DECIMAL_PRECISION);
-        $item['amountNet']     = round($amountNet, UnzerPayment6::MAX_DECIMAL_PRECISION);
+        $item['amountVat'] = round($amountNet * $vat, UnzerPayment6::MAX_DECIMAL_PRECISION);
+        $item['amountNet'] = round($amountNet, UnzerPayment6::MAX_DECIMAL_PRECISION);
 
         return $item;
     }
