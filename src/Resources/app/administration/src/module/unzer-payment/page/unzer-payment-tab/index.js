b8df8457ef4459f202806f6cf16a4a85c248f6f0
Jan-Erik Spreng <j.spreng@kellerkinder.de>
2023-08-04 15:11:38 +0200

    fix: [HEIDELPAYSUPPORT-323] Fix display of wrong order because of different run times of the XHR request.

--- src/Resources/app/administration/src/module/unzer-payment/page/unzer-payment-tab/index.js
+++ src/Resources/app/administration/src/module/unzer-payment/page/unzer-payment-tab/index.js
@@ -15,6 +15,7 @@ Component.register('unzer-payment-tab', {
     data() {
         return {
             paymentResources: [],
+            loadedResources: 0,
             isLoading: true
         };
     },
@@ -43,6 +44,7 @@ Component.register('unzer-payment-tab', {
 
         resetDataAttributes() {
             this.paymentResources = [];
+            this.loadedResources = 0;
             this.isLoading = true;
         },
 
@@ -65,21 +67,26 @@ Component.register('unzer-payment-tab', {
                     return;
                 }
 
-                order.transactions.forEach((orderTransaction) => {
+                order.transactions.forEach((orderTransaction, index) => {
                     if (!orderTransaction.customFields) {
+                        this.loadedResources++;
+
                         return;
                     }
 
                     if (!orderTransaction.customFields.unzer_payment_is_transaction
                         && !orderTransaction.customFields.heidelpay_is_transaction) {
+                        this.loadedResources++;
+
                         return;
                     }
 
                     this.UnzerPaymentService.fetchPaymentDetails(orderTransaction.id)
                         .then((response) => {
-                            this.isLoading = false;
+                            this.paymentResources[index] = response;
+                            this.loadedResources++;
 
-                            this.paymentResources.push(response);
+                            this.isLoading = this.order.transactions.length !== this.loadedResources;
                         })
                         .catch(() => {
                             this.createNotificationError({
